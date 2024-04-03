fed81909683673a3c24a0d15cddc6a8554f7a28f
Ygor Pachiega <73905634+YgorPachiega@users.noreply.github.com>
2024-04-03 14:41:21 -0300

    alteração nome das paginas e adição botão de retorno

--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -1,8 +1,7 @@
 import { FastifyInstance } from 'fastify';
-import { cadastrarUsuario, fazerLogin, puxar } from '../controllers/usersController';
+import { cadastrarUsuario, fazerLogin } from '../controllers/usersController';
 
 export default async function usersRoutes(fastify: FastifyInstance) {
     fastify.post('/cadastro_login', cadastrarUsuario);
     fastify.post('/login', fazerLogin);
-    fastify.post('/puxar', puxar);
 }
