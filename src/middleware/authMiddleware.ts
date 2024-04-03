295c7d7ad1d4e1349ea908f3dd894ad3495f02d5
Ygor Pachiega <73905634+YgorPachiega@users.noreply.github.com>
2024-04-03 10:12:03 -0300

    alteração auth

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,7 +1,10 @@
-import { FastifyRequest, FastifyReply } from 'fastify';
-import { verificarToken } from '../utils/jwtUtils';
-import { JwtPayload } from 'jsonwebtoken';
+// authMiddleware.ts
 
+import { FastifyRequest, FastifyReply } from 'fastify';
+import { verificarToken } from '../utils/jwtUtils'; // Importe a função verificarToken
+import { JwtPayload } from 'jsonwebtoken'; // Importe o tipo JwtPayload
+
+// Declaração de tipos para adicionar a propriedade 'usuario' ao objeto de solicitação
 interface AuthenticatedRequest extends FastifyRequest {
     usuario?: string;
 }
@@ -10,19 +13,24 @@ export const authMiddleware = async (request: AuthenticatedRequest, reply: Fasti
     const token = request.headers['authorization'];
 
     if (!token) {
-        return reply.status(401).send({ error: 'Token não fornecido' });
+        // Se o token não for fornecido, redirecione o usuário para a página de login
+        return reply.redirect('/login');
     }
 
     try {
+        // Use a função verificarToken para validar o token
         const decodedToken = verificarToken(token);
-        if (typeof decodedToken === 'object' && 'usuario' in decodedToken) {
-            request.usuario = (decodedToken as JwtPayload).usuario;
-        } else {
-            throw new Error('Token inválido');
-        }
 
-        done();
+        // Verifique se a variável 'decodedToken' é do tipo 'JwtPayload' antes de acessar a propriedade 'usuario'
+        if (typeof decodedToken === 'object' && 'usuario' in decodedToken) {
+            request.usuario = (decodedToken as JwtPayload).usuario; // Acessa a propriedade 'usuario' do tipo JwtPayload
+            done(); // Continue com a execução da rota
+        } else {
+            // Se 'decodedToken' não for do tipo 'JwtPayload', redirecione o usuário para a página de login
+            return reply.redirect('/login');
+        }
     } catch (error) {
-        return reply.status(401).send({ error: 'Token inválido' });
+        // Se ocorrer um erro ao validar o token, redirecione o usuário para a página de login
+        return reply.redirect('/login');
     }
 };
