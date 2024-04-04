3c9a8b8e22b4c6da80268a4fb38f277548d0176f
Ygor Pachiega <73905634+YgorPachiega@users.noreply.github.com>
2024-04-03 23:02:14 -0300

    ultimo commit antes de comittar

--- src/routes/clientsRoutes.ts
+++ src/routes/clientsRoutes.ts
@@ -3,6 +3,6 @@ import { getCadastro, cadastrarCliente} from '../controllers/clientsController';
 import { authMiddleware } from '../middleware/authMiddleware';
 
 export default async function clientsRoutes(fastify: FastifyInstance) {
-    fastify.get('/cadastro', getCadastro);
-    fastify.post('/cadastrar', cadastrarCliente);
+    fastify.get('/cadastro', { preHandler: authMiddleware }, getCadastro);
+    fastify.post('/cadastrar', { preHandler: authMiddleware }, cadastrarCliente);
 }
