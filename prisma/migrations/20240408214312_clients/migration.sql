6e123e160b9f4dbfdda196427b010fbfab89aefa
Ygor Pachiega <73905634+YgorPachiega@users.noreply.github.com>
2024-04-08 18:43:39 -0300

    Criação de novo migration

--- prisma/migrations/20240408214312_clients/migration.sql
+++ prisma/migrations/20240408214312_clients/migration.sql
@@ -1,16 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Clients` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `Users` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "public"."Clients";
-
--- DropTable
-DROP TABLE "public"."Users";
-
 -- CreateTable
 CREATE TABLE "Users" (
     "id" SERIAL NOT NULL,
