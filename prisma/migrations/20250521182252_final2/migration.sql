9eedf2a129d4cfec84eb3fa45dba0bc4d941da51
Ygor Pachiega <73905634+YgorPachiega@users.noreply.github.com>
2025-05-21 15:23:29 -0300

    final2

---
+++ prisma/migrations/20250521182252_final2/migration.sql
@@ -0,0 +1,71 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Clients` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `empresas` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "public"."users" DROP CONSTRAINT "users_empresaId_fkey";
+
+-- DropTable
+DROP TABLE "public"."Clients";
+
+-- DropTable
+DROP TABLE "public"."empresas";
+
+-- DropTable
+DROP TABLE "public"."users";
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "auth0Id" TEXT NOT NULL,
+    "tipoUsuario" TEXT NOT NULL DEFAULT 'prestador',
+    "empresaRelacionada" TEXT,
+    "aprovado" BOOLEAN NOT NULL DEFAULT false,
+    "empresaId" INTEGER,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "empresas" (
+    "id" SERIAL NOT NULL,
+    "nome" VARCHAR(255) NOT NULL,
+    "cnpj" VARCHAR(14) NOT NULL,
+    "endereco" VARCHAR(255),
+    "setor" VARCHAR(100),
+    "telefone" VARCHAR(20),
+
+    CONSTRAINT "empresas_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Clients" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "empresa" TEXT NOT NULL,
+    "solicitante" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+    "observacao" TEXT
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_auth0Id_key" ON "users"("auth0Id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "empresas_cnpj_key" ON "empresas"("cnpj");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Clients_id_key" ON "Clients"("id");
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_empresaId_fkey" FOREIGN KEY ("empresaId") REFERENCES "empresas"("id") ON DELETE SET NULL ON UPDATE CASCADE;
