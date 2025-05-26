cbc321cbddb17cb139716444c8b30019e38d0e19
Ygor Pachiega <73905634+YgorPachiega@users.noreply.github.com>
2025-05-26 01:14:28 -0300

    Alteracao cors

--- src/middleware/corsMiddleware.ts
+++ src/middleware/corsMiddleware.ts
@@ -3,7 +3,7 @@ import { FastifyRequest, FastifyReply } from 'fastify';
 
 export const corsMiddleware = (request: FastifyRequest, reply: FastifyReply, done: () => void) => {
   reply.header('Access-Control-Allow-Origin', '*');
-  reply.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  reply.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   reply.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
   if (request.method === 'OPTIONS') {
